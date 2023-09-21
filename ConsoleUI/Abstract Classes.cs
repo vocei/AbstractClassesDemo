23e8054032893e9bfb9d66bd214c956eb0d22245
Clifford Myrtil <cliffordhamyrtil@gmail.com>
2023-09-21 16:05:55 -0400

    Abstract Class!

---
+++ ConsoleUI/Abstract Classes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public abstract class Vehicle
+    {
+        public int Year { get; set; } = 1;
+        public string Make { get; set; } = "Default Make";
+        public string Model { get; set; } = "Default Model";
+
+        public abstract void DriveAbstract(); // 0 method
+        
+        public virtual void DriveVirtural()
+        {
+            Console.WriteLine($" This {GetType().Name} is driving virturally");
+        }
+
+    }
+
+    }
